d09c2d2f2e7950e035ddb82f6e7345b4e9799b49
Patatatat <andres.guluarte@gmail.com>
2022-07-01 16:50:19 -0600

    test

---
+++ src/contractAddress.js
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from "react";
+import { ethers } from "ethers";
+import "./App.css";
+
+const App = () => {
+  const [currentAccount, setCurrentAccount] = useState("");
+  /**
+   * Create a variable here that holds the contract address after you deploy!
+   */
+  const contractAddress = "0xb486b64958459f878b6019Dd36B812070f4c47E9";
