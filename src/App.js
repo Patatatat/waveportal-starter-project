94ca6856debc213b46157d782d3fb6f929d7fabf
Patatatat <andres.guluarte@gmail.com>
2022-07-01 21:35:50 -0600

    an emoji

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { ethers } from "ethers";
 import React, { useEffect, useState } from "react";
 import "./App.css";
+import abi from "./utils/WavePortal.json";
 
 const App = () => {
   const [currentAccount, setCurrentAccount] = useState("");
 
   
   const contractAddress = "0xb486b64958459f878b6019Dd36B812070f4c47E9";
+
+  const contractABI = abi.abi;
 
   const checkIfWalletIsConnected = async () => {
     try {
@@ -45,24 +48,7 @@ const App = () => {
         return;
       }
 
-      const wave = async () => {
-        try {
-          const { ethereum } = window;
-    
-          if (ethereum) {
-            const provider = new ethers.providers.Web3Provider(ethereum);
-            const signer = provider.getSigner();
-            const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
-    
-            let count = await wavePortalContract.getTotalWaves();
-            console.log("Retrieved total wave count...", count.toNumber());
-          } else {
-            console.log("Ethereum object doesn't exist!");
-          }
-        } catch (error) {
-          console.log(error);
-        }
-    }
+
 
       const accounts = await ethereum.request({ method: "eth_requestAccounts" });
 
@@ -70,6 +56,38 @@ const App = () => {
       setCurrentAccount(accounts[0]);
     } catch (error) {
       console.log(error)
+    }
+  }
+
+
+  const wave = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+        let count = await wavePortalContract.getTotalWaves();
+        console.log("Retrieved total wave count...", count.toNumber());
+
+        /*
+        * Execute the actual wave from your smart contract
+        */
+        const waveTxn = await wavePortalContract.wave();
+        console.log("Mining...", waveTxn.hash);
+
+        await waveTxn.wait();
+        console.log("Mined -- ", waveTxn.hash);
+
+        count = await wavePortalContract.getTotalWaves();
+        console.log("Retrieved total wave count...", count.toNumber());
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
     }
   }
 
@@ -81,23 +99,24 @@ const App = () => {
     <div className="mainContainer">
       <div className="dataContainer">
         <div className="header">
-        👋 Hey there!
+        👋 Hello 👋!
         </div>
 
         <div className="bio">
-          I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
+          Hello I am Andres and I'm learning how to do a smart contract with solidity and web3 😎. 
+          Connect your Ethereum wallet and wave at me!
         </div>
 
-        <button className="waveButton" onClick={null}>
-          Wave at Me
+        <button className="waveButton" onClick={wave}>
+          Wave
         </button>
 
         {/*
         * If there is no currentAccount render this button
         */}
         {!currentAccount && (
-          <button className="waveButton" onClick={wave}>
-          Wave at Me
+          <button className="waveButton" onClick={connectWallet}>
+         Connect
           </button>
       
         )}
